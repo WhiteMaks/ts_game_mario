a9204c44eb7a335de50daa0f38925528ffe8e26d
WhiteMaks <whitemaks.working@gmail.com>
2024-02-10 22:48:46 +0200

    init empty project

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const parentElement = document.getElementById("game");
+if (!parentElement) {
+	throw new Error("Game element nof found");
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.ts',
+	module: {
+		rules: [
+			{
+				test: /\.ts?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+					},
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'build'),
+	},
+	devServer: {
+		static: path.join(__dirname, "build"),
+		compress: true,
+		port: 4000,
+	},
+};
